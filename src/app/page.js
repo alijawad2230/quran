788567cf9912dbdaf0112e4043fb03dc4bba608a
Alijawad Channa <Alijawadchanna2230@gmail.com>
2023-05-12 17:13:45 +0430

    1

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline text-rose-400">Hello world!</h1>
+    <main className="bg-slate-900 text-gray-100 p-96">
+      <h1 className="text-3xl font-bold underline text-rose-400">
+        Hello world!
+      </h1>
     </main>
   );
 }
