c5678d0fc996224c533cf19d735217b903fad911
Alijawad Channa <Alijawadchanna2230@gmail.com>
2023-05-13 00:18:51 +0430

    fix

--- src/components/AyatText.js
+++ src/components/AyatText.js
@@ -2,20 +2,23 @@
 
 export default function AyatText(props) {
   return (
-    <div className="flex" dir="rtl">
-      <span className="font-sans"> {props.text} </span>
-      <div className="relative w-10 items-center justify-center">
-        <img className="items-center justify-center" src="/ayatmark.png" />
-        <span
-          className="absolute "
-          style={{
-            left: "50%",
-            top: "50%",
-            transform: "translate(-50%, -50%)",
-          }}
-        >
-          {props.ayat}
-        </span>
+    <div className="" dir="rtl">
+      <div className="px-2">
+        {" "}
+        {props.text}{" "}
+        <div className="relative w-16 my-auto">
+          <img className="items-center justify-center " src="/ayatmark.png" />
+          <span
+            className="absolute "
+            style={{
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            {props.ayat}
+          </span>
+        </div>
       </div>
     </div>
   );
