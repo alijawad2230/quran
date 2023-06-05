742df7ab37822a88bf00cf54167b6dfa618c7577
Alijawad Channa <Alijawadchanna2230@gmail.com>
2023-06-06 03:19:52 +0430

    some edit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // add the lines below ↓
-  output: "standalone",
+  experimental: {
+    serverActions: true,
+  },
 };
 // module.exports = nextConfig;
 module.exports = nextConfig;
