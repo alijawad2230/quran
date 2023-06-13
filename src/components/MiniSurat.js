b8853c9ae56db20db6c76d83607b61146deebe40
Alijawad Channa <Alijawadchanna2230@gmail.com>
2023-06-13 07:22:53 +0430

    dark

--- src/components/MiniSurat.js
+++ src/components/MiniSurat.js
@@ -6,7 +6,7 @@ export default function MiniSurat(props) {
   return (
     <div
       onClick={movetosurat}
-      class="cursor-pointer flex flex-row rounded-lg bg-white  dark:bg-neutral-700 w-full font-sans"
+      class="cursor-pointer flex flex-row rounded-lg bg-neutral-700 w-full font-sans"
     >
       <div className="relative w-40 flex items-center justify-center">
         <img src="surat.png " className="" />
@@ -22,16 +22,16 @@ export default function MiniSurat(props) {
         </span>
       </div>
       <div dir="rtl" class="flex flex-col justify-start p-6 w-full">
-        <span class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+        <span class="mb-2 text-xl font-medium  text-neutral-50">
           نام : <span>{props.data.arabicname}</span>
           <span className="hidden md:block">
             {props.data.name} ({props.data.englishname})
           </span>
         </span>
-        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        <p class="mb-4 text-base text-neutral-600 ">
           تعداد ایات : {props.data.verses.length}
         </p>
-        <p class="text-xs text-neutral-500 dark:text-neutral-300">
+        <p class="text-xs text-neutral-500 ">
           نازل شده در : {props.data.revelation}
         </p>
       </div>
